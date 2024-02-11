405306369ec14285f48579ea7806c91500e5809c
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-11 13:48:11 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <Script src="\/\/crookedchange.com\/bpXhVCsjd.Gtlj0QY\/WkdOisYhW\/5juaZUXeIF\/Xepmq9OuCZfUGlXkAPwTcQR4GM\/zJU\/1_NYTfEXteNTDggCzcN_TWUv1\/MzgI"/>
+  <script async='async' src='//adserver.juicyads.com/js/jads.js'/><ins data-height='250' data-width='300' id='678585'/><script>(adsbyjuicy = window.adsbyjuicy || []).push({&#39;adzone&#39;:678585});</script>
 );
 
 export default AdScript;
