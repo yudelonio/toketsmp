11f151e60fb2a43ced1ac7fcb530b020af068453
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-11 14:09:41 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <script async='async' src='//adserver.juicyads.com/js/jads.js'/><ins data-height='250' data-width='300' id='678585'/><script>(adsbyjuicy = window.adsbyjuicy || []).push({&#39;adzone&#39;:678585});</script>
+  &lt;script async=&#039;async&#039; src=&#039;//adserver.juicyads.com/js/jads.js&#039;/&gt;&lt;ins data-height=&#039;250&#039; data-width=&#039;300&#039; id=&#039;678585&#039;/&gt;&lt;script&gt;(adsbyjuicy = window.adsbyjuicy || []).push({&amp;#39;adzone&amp;#39;:678585});&lt;/script&gt;
 );
 
 export default AdScript;
