7362ef7d21a0583038352b8393799290e897c286
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-12 01:42:24 +0700

    Update center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import AdScript from "@/components/ads";
 
 const Center = ({ children }: { children: React.ReactNode }) => {
     return (
-        <AdScript />
          <div className="flex flex-col h-[50vh] justify-center items-center">
             {children}
         </div>
