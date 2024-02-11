c25f7d2a59f86aaa39b1b750e29dac29933090f7
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-11 14:56:46 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
- <Script src="\/\/crookedchange.com\/bpXhVCsjd.Gtlj0QY\/WkdOisYhW\/5juaZUXeIF\/Xepmq9OuCZfUGlXkAPwTcQR4GM\/zJU\/1_NYTfEXteNTDggCzcN_TWUv1\/MzgI"/>
+  <Script src="\/\/crookedchange.com\/bpXhVCsjd.Gtlj0QY\/WkdOisYhW\/5juaZUXeIF\/Xepmq9OuCZfUGlXkAPwTcQR4GM\/zJU\/1_NYTfEXteNTDggCzcN_TWUv1\/MzgI"/>
 );
 
 export default AdScript;
