48440c03f1eabf107883974674ca8d0fd7d2c209
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-11 14:39:59 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <script src='\/\/js.juicyads.com\/jp.php?c=947403z2v256s2p2w28403c4&amp;u=http%3A%2F%2Fwww.juicyads.rock'>
+  <script src='\/\/js.juicyads.com\/jp.php?c=947403z2v256s2p2w28403c4&amp;u=http%3A%2F%2Fwww.juicyads.rock'/>
 );
 
 export default AdScript;
