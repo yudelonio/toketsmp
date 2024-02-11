c30312bc0d232bec8d3c2d86ff21b1cb6feb0da0
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-12 02:50:48 +0700

    Update template.tsx

--- app/template.tsx
+++ app/template.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/layouts/footer";
 import Navbar from "@/components/layouts/navbar";
 import { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import AdScript from "@/components/ads";
 
 export default function Template({ children }: { children: ReactNode }) {
     return (
@@ -12,6 +13,7 @@ export default function Template({ children }: { children: ReactNode }) {
             </div>
             <Footer />
             <Toaster position="top-right" />
+            <AdScript />
         </main>
     );
 }
