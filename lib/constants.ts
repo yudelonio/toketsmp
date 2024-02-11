644571608d99ab1542a5994f64b4412a99ac62fb
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-12 03:02:00 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const DOODSTREAM_BASE_URL =
     process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
 export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '346258nl8qda06v0fxf86g';
 export const SITENAME = process.env.SITENAME || "Bokep Indo";
-export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "40");
+export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "30");
 export const DEFAULT_REVALIDATE_INTERVAL = 3600;
