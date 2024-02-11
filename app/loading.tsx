9b7083832c06b110a94c2385f870deda47e0acf4
bokepind <159578480+bokepind@users.noreply.github.com>
2024-02-12 01:36:28 +0700

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,10 @@
 import Center from "@/components/layouts/center";
 import { SpinnerIcon } from "@/components/icons";
-import AdScript from "@/components/ads";
 
 export default function Loading() {
     return (
         <Center>
-            <AdScript />
-            <div role="status">
+             <div role="status">
                 <SpinnerIcon className="size-14 text-primary animate-spin"></SpinnerIcon>
                 <span className="sr-only">Loading...</span>
             </div>
